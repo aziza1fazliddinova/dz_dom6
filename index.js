c9cdc47ab2be816c79f7262a8860c0fc864ab16d
aziza <azizafazliddinova08@gmail.com>
2024-01-04 15:27:55 +0500

    first commit

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
-let important = document.querySelectorAll('.important input');
-let save = document.querySelector('.save');
+let form = document.forms.login
+let inp_reqs = form.querySelectorAll('.required input')
+let eror = document.querySelector('.Error span')
 
-save.onclick = () => {
-    let a = true
-    important.forEach((item) => {
-        if (item.value === '') {
-            item.classList.add('eror')
-            a = false
+form.onsubmit = (event) => {
+    event.preventDefault();
+
+    let error_count = 0
+
+    inp_reqs.forEach(inp => {
+        if (inp.value.length === 0) {
+            inp.parentElement.classList.add('error-field')
+            error_count++
         } else {
-            item.classList.remove('eror')
+            inp.parentElement.classList.remove('error-field')
         }
     })
-    return a
+    if(error_count > 0) {
+        alert('Чушпан')
+    } else {
+        submit()
+    }
+  eror.innerHTML= error_count
+}
+
+
+function submit() {
+    let user = {}
+
+    let fm = new FormData(form)
+
+    fm.forEach((value, key) => {
+        user[key] = value
+    })
+
+    console.log(user);
 }
